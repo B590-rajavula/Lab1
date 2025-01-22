e32781532db94534ede162ef4b6c42995b31c5b7
Rajesh Kumar Reddy Avula <rajeshkumarreddyavula@Rajeshs-MacBook-Air-379.local>
2025-01-22 01:31:52 -0500

    lab2-challenge1

--- app/src/main/java/iu/b590/spring2025/lab1/MainActivity.kt
+++ app/src/main/java/iu/b590/spring2025/lab1/MainActivity.kt
@@ -37,21 +37,28 @@ class MainActivity : AppCompatActivity() {
 //            view: View ->
         binding.trueButton.setOnClickListener{
             view: View ->
-            Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+//            Toast.makeText(
+//                this,
+//                R.string.correct_toast,
+//                Toast.LENGTH_SHORT
+//            ).show()
+            checkAnswer(true)
         }
         binding.falseButton.setOnClickListener{
                 view: View ->
-            Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+//            Toast.makeText(
+//                this,
+//                R.string.correct_toast,
+//                Toast.LENGTH_SHORT
+//            ).show()
+            checkAnswer(false)
         }
         binding.nextButton.setOnClickListener{
+            currentIndex = (currentIndex+1)%questionBank.size
+            updateQuestion()
+        }
+
+        binding.questionTextView.setOnClickListener{
             currentIndex = (currentIndex+1)%questionBank.size
             updateQuestion()
         }
@@ -63,5 +70,15 @@ class MainActivity : AppCompatActivity() {
     private  fun updateQuestion(){
         val questionTextResId = questionBank[currentIndex].textResId
         binding.questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer:Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
 }
