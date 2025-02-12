e4982b53b356de6f57cdb58d066dba3c6a9609b6
Rajesh112k <sairajesh112k@gmail.com>
2025-02-11 21:29:57 -0500

    Lab5

---
+++ app/src/test/java/android/util/Log.java
@@ -0,0 +1,34 @@
+package android.util;
+
+public class Log {
+    public static int d(String tag, String msg) {
+        System.out.println("DEBUG: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int d(String tag, String msg, Throwable tr) {
+        System.out.println("DEBUG: " + tag + ": " + msg);
+//        tr.printStackTrace();  // Print stack trace for debugging
+        return 0;
+    }
+
+    public static int i(String tag, String msg) {
+        System.out.println("INFO: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int w(String tag, String msg) {
+        System.out.println("WARN: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int e(String tag, String msg) {
+        System.out.println("ERROR: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int e(String tag, String msg, Throwable tr) {
+        System.out.println("ERROR: " + tag + ": " + msg);// Print stack trace for debugging
+        return 0;
+    }
+}
