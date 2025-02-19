ebc4180f0e9543bebaeb477ebf6aa8fd0fe103a9
Rajesh112k <sairajesh112k@gmail.com>
2025-02-19 00:02:38 -0500

    Lab7

---
+++ Documents/Spring_2025/practicum7/app/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "iu.b590.spring2025.practicum7"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "iu.b590.spring2025.practicum7"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
+//        dataBinding = true  // Add this
+    }
+}
+
+dependencies {
+
+
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.androidx.appcompat)
+        implementation(libs.material)
+        implementation(libs.androidx.activity)
+        implementation(libs.androidx.constraintlayout)
+    debugImplementation ("androidx.fragment:fragment-testing:1.8.3")
+
+    testImplementation(libs.junit)
+
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation ("androidx.test.espresso:espresso-contrib:3.5.1")
+
+
+
+
+}
