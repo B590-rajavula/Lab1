99a97dc78d04b53bfd24272a5e4b5dda0fbea288
Rajesh112k <sairajesh112k@gmail.com>
2025-02-20 15:04:53 -0500

    Lab7

--- app/src/androidTest/java/iu/b590/spring2025/practicum7/TicketDetailFragmentTest.kt
+++ app/src/androidTest/java/iu/b590/spring2025/practicum7/TicketDetailFragmentTest.kt
@@ -6,10 +6,13 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isChecked
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import org.junit.Test
+import org.junit.runner.RunWith
 
-
+@RunWith(AndroidJUnit4::class)
 class TicketDetailFragmentTest{
     @Test
     fun checktextandBoxupdate() {
