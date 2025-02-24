6f046da90fa7479d82e248a66409c453fcae10ef
Rajesh112k <sairajesh112k@gmail.com>
2025-02-24 13:46:43 -0500

    day7

---
+++ app/src/main/java/iu/b590/spring2025/day7/Main.kt
@@ -0,0 +1,31 @@
+package iu.b590.spring2025.day7
+
+fun main(){
+    milkCows()
+    feedChickens()
+}
+
+fun milkCows(){
+    var cow = 1
+    println("Milking cow #${cow}")
+    cow+=1
+    println("Milking cow #${cow}")
+    cow+=1
+    println("Milking cow #${cow}")
+    cow+=1
+    println("Milking cow #${cow}")
+    cow+=1
+}
+
+fun feedChickens(){
+    var chicken = 1
+    println("Feeding chicken #${chicken}")
+    chicken +=1
+    println("Feeding chicken #${chicken}")
+    chicken +=1
+    println("Feeding chicken #${chicken}")
+    chicken +=1
+    println("Feeding chicken #${chicken}")
+    chicken +=1
+
+}
