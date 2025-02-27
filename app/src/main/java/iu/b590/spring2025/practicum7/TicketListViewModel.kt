e267fa5d00ef0ba63a1b14f40d623fc61c66be0f
Rajesh112k <sairajesh112k@gmail.com>
2025-02-27 01:02:42 -0500

    homework3-part2

---
+++ app/src/main/java/iu/b590/spring2025/practicum7/TicketListViewModel.kt
@@ -0,0 +1,20 @@
+package iu.b590.spring2025.practicum7
+
+import androidx.lifecycle.ViewModel
+import java.util.Date
+import java.util.UUID
+
+class TicketListViewModel: ViewModel() {
+    val tickets = mutableListOf<Ticket>()
+    init{
+        for(i in 0 until 100) {
+            val ticket = Ticket(
+                id = UUID.randomUUID()  ,
+                title = "ticket #$i",
+                date = Date(),
+                isSolved = i%2 == 0
+            )
+            tickets += ticket
+        }
+    }
+}
