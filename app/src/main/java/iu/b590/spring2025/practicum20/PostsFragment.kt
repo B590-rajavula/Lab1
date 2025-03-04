d5a62ae96226bf172e368105e1fcf56779bef8b9
Rajesh112k <sairajesh112k@gmail.com>
2025-03-04 17:50:26 -0500

    midterm-section2-part2-challenge1

--- app/src/main/java/iu/b590/spring2025/practicum20/PostsFragment.kt
+++ app/src/main/java/iu/b590/spring2025/practicum20/PostsFragment.kt
@@ -2,8 +2,10 @@ package iu.b590.spring2025.practicum20
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -13,6 +15,9 @@ import com.google.firebase.auth.FirebaseAuth
 import androidx.navigation.fragment.findNavController
 import iu.b590.spring2025.practicum20.databinding.FragmentPostsBinding
 import kotlinx.coroutines.launch
+import androidx.appcompat.widget.Toolbar
+import android.view.Menu
+import android.view.MenuInflater
 
 class PostsFragment : Fragment() {
 
@@ -23,69 +28,67 @@ class PostsFragment : Fragment() {
         }
     private val postViewModel: PostViewModel by viewModels()
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPostsBinding.inflate(inflater, container, false)
 
-override fun onCreateView(
-inflater: LayoutInflater,
-container: ViewGroup?,
-savedInstanceState: Bundle?
-): View? {
-// Inflate the layout for this fragment
-    _binding = FragmentPostsBinding.inflate(inflater, container,  false)
+        // Set up the Toolbar as the action bar
+        setHasOptionsMenu(true)
 
-    return binding.root
-}
-override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated (view, savedInstanceState)
+        // Initialize the toolbar
+        val toolbar = binding.root.findViewById<Toolbar>(R.id.toolbar)
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
 
-    viewLifecycleOwner.lifecycleScope.launch {
-        viewLifecycleOwner.repeatOnLifecycle(
-            Lifecycle.State.STARTED)
-        {
-            postViewModel.posts.collect { posts ->
-                binding.rvPosts.adapter = PostsAdapter(posts)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Collect posts data
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                postViewModel.posts.collect { posts ->
+                    binding.rvPosts.adapter = PostsAdapter(posts)
+                }
             }
         }
-    }
-    val auth = FirebaseAuth.getInstance()
-    if (auth.currentUser == null) {
-        goToLoginScreen()
+
+        // Check if user is logged in, else go to login screen
+        val auth = FirebaseAuth.getInstance()
+        if (auth.currentUser == null) {
+            goToLoginScreen()
+        }
+
+        binding.fabCreate.setOnClickListener {
+            this.findNavController().navigate(R.id.navigate_to_createFragment)
+        }
     }
 
-    binding.btnLogout.setOnClickListener{
-        auth.signOut()
-        goToLoginScreen()
+    // Inflates the menu to add the logout option
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_posts, menu) // Inflate your menu
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
-    binding.fabCreate.setOnClickListener {
-        this.findNavController().navigate(R.id.navigate_to_createFragment)
+
+    // Handle the logout action from the toolbar
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_logout -> {
+                FirebaseAuth.getInstance().signOut()
+                goToLoginScreen()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun goToLoginScreen() {
+        this.findNavController().navigate(R.id.navigateToLogin)
     }
 }
-private fun goToLoginScreen() {
-    this.findNavController().navigate(R.id.navigateToLogin)
-}
-}
 
-
-
-
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        _binding = FragmentPostsBinding.inflate(inflater, container, false)
-//
-//        binding.btnLogout.setOnClickListener {
-//            FirebaseAuth.getInstance().signOut()
-//            findNavController().navigate(R.id.navigateToLogin)
-////            To Navigate to login Page
-//        }
-//
-//        return binding.root
-//    }
-//
-//    override fun onDestroyView() {
-//        super.onDestroyView()
-//        _binding = null
-//    }
-//}
