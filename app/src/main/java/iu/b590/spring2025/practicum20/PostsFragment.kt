72de64439c4da3e7ec64c8336228e0c4696cdbfe
Rajesh112k <sairajesh112k@gmail.com>
2025-03-04 00:25:47 -0500

    midterm-section2

---
+++ app/src/main/java/iu/b590/spring2025/practicum20/PostsFragment.kt
@@ -0,0 +1,39 @@
+package iu.b590.spring2025.practicum20
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import androidx.navigation.fragment.findNavController
+import iu.b590.spring2025.practicum20.databinding.FragmentPostsBinding
+
+class PostsFragment : Fragment() {
+
+    private var _binding: FragmentPostsBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null."
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPostsBinding.inflate(inflater, container, false)
+
+        binding.btnLogout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            findNavController().navigate(R.id.navigateToLogin)
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
