1e80a492773b59b5287d28d017d8b85a8b6424c8
Rajesh112k <sairajesh112k@gmail.com>
2025-03-05 21:08:06 -0500

    midterm-section4

--- app/src/main/java/iu/b590/spring2025/midtermsection4/MainActivity.kt
+++ app/src/main/java/iu/b590/spring2025/midtermsection4/MainActivity.kt
@@ -65,7 +65,6 @@ class MainActivity : AppCompatActivity() {
             .before (nightSkyAnimator)
         animatorSet.start()
 
-//        heightAnimator.start()
-//        sunsetSkyAnimator.start()
+
     }
 }
