221ae835ef26f3fd7523a27862ecebbde493cc2c
Rajesh112k <sairajesh112k@gmail.com>
2025-03-05 19:58:04 -0500

    midterm-section3

---
+++ app/src/main/java/iu/b590/spring2025/midtermsection3/MainActivity.kt
@@ -0,0 +1,16 @@
+package iu.b590.spring2025.midtermsection3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+    }
+}
