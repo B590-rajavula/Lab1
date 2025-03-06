2242941232f34887ccdac4bcb8130be861c6718a
Rajesh112k <sairajesh112k@gmail.com>
2025-03-06 01:17:42 -0500

    midterm-section5-part2

--- app/src/main/java/iu/b590/spring2025/midtermsection5/ui/PizzaBuilderScreen.kt
+++ app/src/main/java/iu/b590/spring2025/midtermsection5/ui/PizzaBuilderScreen.kt
@@ -1,5 +1,7 @@
 package iu.b590.spring2025.midtermsection5.ui
 
+import android.icu.text.NumberFormat
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,8 +17,14 @@ import iu.b590.spring2025.midtermsection5.R
 import iu.b590.spring2025.midtermsection5.model.Topping
 import iu.b590.spring2025.midtermsection5.model.ToppingPlacement
 import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.unit.dp
+import iu.b590.spring2025.midtermsection5.model.Pizza
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 
 
 @Preview
@@ -24,15 +32,19 @@ import androidx.compose.ui.unit.dp
 fun PizzaBuilderScreen(
     modifier: Modifier = Modifier
 ) {
+    var pizza by rememberSaveable { mutableStateOf(Pizza()) }
     Column(
         modifier = modifier
     ) {
         ToppingsList(
+            pizza = pizza,
+            onEditPizza = {pizza = it},
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(1f, fill = true)
         )
         OrderButton(
+            pizza = pizza,
             modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
@@ -40,10 +52,25 @@ fun PizzaBuilderScreen(
     }
 }
 
+//private var pizza =
+//    Pizza(
+//        toppings = mapOf(
+//            Topping.Pepperoni to ToppingPlacement.All,
+//            Topping.Pineapple to ToppingPlacement.All
+//        )
+//    )
+//    set(value) {
+//        Log.d("PizzaBuilderScreen", "Reassigned pizza to $value")
+//        field = value
+//    }
+
+//private var pizza by mutableStateOf(Pizza())
 
 
 @Composable
 private fun ToppingsList(
+    pizza: Pizza,
+    onEditPizza: (Pizza) -> Unit,
     modifier: Modifier = Modifier
 ) {
 //    ToppingCell(
@@ -52,6 +79,23 @@ private fun ToppingsList(
 //        onClickTopping = {},
 //        modifier = modifier
 //    )
+//    var pizza by remember { mutableStateOf(Pizza()) }
+
+//    var showToppingPlacementDialog by rememberSaveable { mutableStateOf(false) }
+var toppingBeingAdded by rememberSaveable { mutableStateOf<Topping?>(null) }
+//    if(showToppingPlacementDialog) {
+    toppingBeingAdded?.let{topping ->
+        ToppingPlacementDialog (
+            topping = topping,
+            onSetToppingPlacement = {placement ->
+                onEditPizza(pizza.withTopping(topping, placement))
+            },
+            onDismissRequest = {
+//                showToppingPlacementDialog = false
+                toppingBeingAdded = null
+            }
+        )
+    }
 
     LazyColumn (
         modifier = modifier
@@ -59,9 +103,21 @@ private fun ToppingsList(
         items (Topping.values()) { topping ->
             ToppingCell(
                 topping = topping,
-                placement = ToppingPlacement.Left,
+//                placement = ToppingPlacement.Left,
+                placement = pizza.toppings[topping],
                 onClickTopping = {
-
+//                    val isOnPizza = pizza.toppings[topping] != null
+//                    pizza =
+//                        onEditPizza(pizza.withTopping(
+//                        topping = topping,
+//                        placement = if (isOnPizza) {
+//                            null
+//                        } else {
+//                            ToppingPlacement.All
+//                        }
+//                    ))
+//                    showToppingPlacementDialog = true
+                    toppingBeingAdded = topping
                 }
             )
         }
@@ -70,6 +126,7 @@ private fun ToppingsList(
 
 @Composable
 private fun OrderButton(
+    pizza: Pizza,
     modifier: Modifier = Modifier
 ) {
     Button(
@@ -78,8 +135,10 @@ private fun OrderButton(
 
         }
     ) {
+        val currencyFormatter = remember { NumberFormat.getCurrencyInstance() }
+        val price = currencyFormatter.format(pizza.price)
         Text(
-            text = stringResource(R.string.place_order_button)
+            text = stringResource(R.string.place_order_button, price)
                 .uppercase()
         )
     }
--- app/src/main/java/iu/b590/spring2025/midtermsection5/ui/ToppingCell.kt
+++ app/src/main/java/iu/b590/spring2025/midtermsection5/ui/ToppingCell.kt
@@ -55,7 +55,7 @@ fun ToppingCell(
         Checkbox(
 //            checked = true,
             checked = (placement != null),
-            onCheckedChange = {}
+            onCheckedChange = {onClickTopping()}
         )
 
         Column (
